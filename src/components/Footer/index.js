857a300bb3bacef3c41cd6fb82c4e062d8056729
Joe Rehfuss <joe.rehfuss@gmail.com>
2021-05-24 12:16:27 -0700

    wip

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+      <footer>
+        <div>
+          <h3>UP boutique, inc.</h3>
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
